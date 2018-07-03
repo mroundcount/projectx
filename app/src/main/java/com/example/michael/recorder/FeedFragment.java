7c2b809221924df88c2cb3a6a0f2e941648ed52b
Lucas <rydberg@slu.edu>
2018-07-03 08:18:28 -0500

    fragments and container

--- app/src/main/java/com/example/michael/recorder/FeedFragment.java
+++ app/src/main/java/com/example/michael/recorder/FeedFragment.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
